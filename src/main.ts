df8c15ec7073608936d143c63d7b28c38a7fbd4d
gabrielm2q <200018167@aluno.unb.br>
2022-03-27 13:43:02 -0300

    Config inicial do menu

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// Importações
 import { Educacao } from './model/Educacao'
 import { Alimentacao } from './model/Alimentacao'
 import { Entretenimento } from './model/Entretenimento'
@@ -5,25 +6,33 @@ import { Saude } from './model/Saude'
 import { Transporte } from './model/Transporte'
 import { Ganho } from './model/Ganho'
 
-//import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-//const rl = readline.createInterface({ input, output });
 
+// Declarações
 let gastos: Array<Educacao | Alimentacao | Entretenimento | Saude | Transporte> = []
 let ganhos: Array<Ganho> = []
+let opcao: number = 0
 
+// npm install prompt-sync
+const prompt = require("prompt-sync")()
 
-
-function imprimeLinha(tamanho = 25){
-    for(let i = 1; i < tamanho; i++){
-        process.stdout.write("-");
+// Função para imprimir uma linha do tamanho desejado
+function imprimeLinha(tamanho = 40, tipo = 1){
+    if(tipo === 1){
+        for(let i = 1; i < tamanho; i++){
+            process.stdout.write("-");
+        }
+        console.log("-")
+    } else {
+        for(let i = 1; i < tamanho; i++){
+            process.stdout.write("=");
+        }
+        console.log("=")
     }
-    console.log("-")
 }
 
-
+// Função para imprimir o menu e pegar a opção escolhida
 function imprimeMenu() {
-    console.log("---------------MENU---------------")
+    console.log("------------------MENU------------------")
     console.log("1 - ADICIONAR GASTO")
     console.log("2 - REMOVER GASTO")
     console.log("3 - LISTAR GASTOS")
@@ -33,11 +42,43 @@ function imprimeMenu() {
     console.log("7 - LISTAR GANHOS")
     console.log("8 - EDITAR GANHO")
     console.log("9 - ENCERRAR")
+
+    opcao = Number(prompt("Opção escolhida: "))
 }
 
-imprimeMenu()
-
-//let entrada = rl.question("Opção escolhida: ")
-//rl.close()
-
-//console.log(entrada)
+// Começo do programa
+imprimeLinha(40,2)
+do{
+    imprimeMenu()
+    imprimeLinha(40, 1)
+    switch (opcao) {
+        case 1:
+            console.log("~~~~~~~~~~ADICIONAR GASTO~~~~~~~~~~")
+            break;
+        case 2:
+            console.log("~~~~~~~~~~REMOVER GASTO~~~~~~~~~~")
+            break;
+        case 3:
+            console.log("~~~~~~~~~~LISTAR GASTOS~~~~~~~~~~")
+            break;
+        case 4:
+            console.log("~~~~~~~~~~EDITAR GASTOS~~~~~~~~~~")
+            break;
+        case 5:
+            console.log("~~~~~~~~~~ADICIONAR GANHO~~~~~~~~~~")
+            break;
+        case 6:
+            console.log("~~~~~~~~~~REMOVER GANHO~~~~~~~~~~")
+            break;
+        case 7:
+            console.log("~~~~~~~~~~LISTAR GANHOS~~~~~~~~~~")
+            break;
+        case 8:
+            console.log("~~~~~~~~~~EDITAR GANHO~~~~~~~~~~")
+            break;
+        default:
+            console.log("OBRIGADO POR UTILIZAR O PROGRAMA!")
+            break;
+    }
+    imprimeLinha(40, 2)
+} while (opcao != 9)
