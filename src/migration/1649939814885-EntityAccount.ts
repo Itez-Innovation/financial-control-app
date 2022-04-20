602dd526cd417d305a2304722828748bc0b90adf
gabrielm2q <mariano.gabriel2q@gmail.com>
2022-04-19 23:01:37 -0300

    :fire: comentando migrations não usadas

--- src/migration/1649939814885-EntityAccount.ts
+++ src/migration/1649939814885-EntityAccount.ts
@@ -1,64 +1,64 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+// import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class EntityAccount1649939814885 implements MigrationInterface {
+// export class EntityAccount1649939814885 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'")
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'")
         
-        await queryRunner.createTable(
-            new Table(
-                {
-                    name: "accounts",
-                    columns: [
-                        {
-                            name: "id",
-                            generationStrategy: 'uuid',
-                            type: 'uuid',
-                            isPrimary: true,
-                            isUnique: true,
-                            isGenerated: true,
-                            default: "uuid_generate_v4()"
-                        },
-                        {
-                            name: "cpf",
-                            type: "varchar",
-                            length: "14",
-                            isUnique: true
-                        },
-                        {
-                            name: "name",
-                            type: "varchar"
-                        },
-                        {
-                            name: "createdAt",
-                            type: "timestamp",
-                            default: "now()"
-                        },
-                        {
-                            name: "updatedAt",
-                            type: "timestamp",
-                            default: "now()"
-                        },
-                        {
-                            name: "inputs",
-                            type: "uuid",
-                            isNullable: true
-                        },
-                        {
-                            name: "outputs",
-                            type: "uuid",
-                            isNullable: true
-                        }
-                    ]
-                }
-            )
-        )
-    }
+//         await queryRunner.createTable(
+//             new Table(
+//                 {
+//                     name: "accounts",
+//                     columns: [
+//                         {
+//                             name: "id",
+//                             generationStrategy: 'uuid',
+//                             type: 'uuid',
+//                             isPrimary: true,
+//                             isUnique: true,
+//                             isGenerated: true,
+//                             default: "uuid_generate_v4()"
+//                         },
+//                         {
+//                             name: "cpf",
+//                             type: "varchar",
+//                             length: "14",
+//                             isUnique: true
+//                         },
+//                         {
+//                             name: "name",
+//                             type: "varchar"
+//                         },
+//                         {
+//                             name: "createdAt",
+//                             type: "timestamp",
+//                             default: "now()"
+//                         },
+//                         {
+//                             name: "updatedAt",
+//                             type: "timestamp",
+//                             default: "now()"
+//                         },
+//                         {
+//                             name: "inputs",
+//                             type: "uuid",
+//                             isNullable: true
+//                         },
+//                         {
+//                             name: "outputs",
+//                             type: "uuid",
+//                             isNullable: true
+//                         }
+//                     ]
+//                 }
+//             )
+//         )
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("accounts")
-        await queryRunner.dropTable("inputs")
-        await queryRunner.dropTable("outputs")
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.dropTable("accounts")
+//         await queryRunner.dropTable("inputs")
+//         await queryRunner.dropTable("outputs")
+//     }
 
-}
+// }
--- src/migration/1649942194912-EntityCashInflow.ts
+++ src/migration/1649942194912-EntityCashInflow.ts
@@ -1,62 +1,62 @@
-import {MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+// import {MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
-export class EntityCashInflow1649942194912 implements MigrationInterface {
+// export class EntityCashInflow1649942194912 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'")
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'")
         
-        await queryRunner.createTable(
-            new Table(
-                {
-                    name: "inputs",
-                    columns: [
-                        {
-                            name: "id",
-                            generationStrategy: 'uuid',
-                            type: 'uuid',
-                            isPrimary: true,
-                            isUnique: true,
-                            isGenerated: true,
-                            default: "uuid_generate_v4()"
-                        },
-                        {
-                            name: "titulo",
-                            type: "varchar",
-                            default: "Recebido", 
-                        },
-                        {
-                            name: "valor",
-                            type: "float(4)",
-                            isNullable: false,
-                        },
-                        {
-                            name: "createdAt",
-                            type: "timestamp",
-                            default: "now()"
-                        },
-                        {
-                            name: "updatedAt",
-                            type: "timestamp",
-                            default: "now()"
-                        },
-                    ]
-                }
-            )
-        )
+//         await queryRunner.createTable(
+//             new Table(
+//                 {
+//                     name: "inputs",
+//                     columns: [
+//                         {
+//                             name: "id",
+//                             generationStrategy: 'uuid',
+//                             type: 'uuid',
+//                             isPrimary: true,
+//                             isUnique: true,
+//                             isGenerated: true,
+//                             default: "uuid_generate_v4()"
+//                         },
+//                         {
+//                             name: "titulo",
+//                             type: "varchar",
+//                             default: "Recebido", 
+//                         },
+//                         {
+//                             name: "valor",
+//                             type: "float(4)",
+//                             isNullable: false,
+//                         },
+//                         {
+//                             name: "createdAt",
+//                             type: "timestamp",
+//                             default: "now()"
+//                         },
+//                         {
+//                             name: "updatedAt",
+//                             type: "timestamp",
+//                             default: "now()"
+//                         },
+//                     ]
+//                 }
+//             )
+//         )
 
-        await queryRunner.createForeignKey(
-            "accounts",
-            new TableForeignKey({
-                columnNames: ["inputs"],
-                referencedTableName: "cashInflow",
-                referencedColumnNames: ["id"],
-                onDelete: "CASCADE"                
-            })
-        )
-    }
+//         await queryRunner.createForeignKey(
+//             "accounts",
+//             new TableForeignKey({
+//                 columnNames: ["inputs"],
+//                 referencedTableName: "cashInflow",
+//                 referencedColumnNames: ["id"],
+//                 onDelete: "CASCADE"                
+//             })
+//         )
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("inputs")
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.dropTable("inputs")
+//     }
 
-}
+// }
