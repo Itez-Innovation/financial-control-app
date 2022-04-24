3f6b2b5a33f168a16bb131b8912a610d194555c0
gabrielm2q <mariano.gabriel2q@gmail.com>
2022-04-24 18:10:28 -0300

    :repeat: refatoração dos menus I (não funcional)

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,12 @@ import AccountRepository from './repositories/AccountRepository';
 const app = async () =>{
         // Declarações
         let option = 0
-        let idAcc = 0
-        let idOutflow = 0
-        let idInflow = 0
 
         const accountRepository = new AccountRepository()
-        
-
+        let ac = new AccountEntity()
 
         // npm install prompt-sync
         const prompt = require("prompt-sync")()
-
-        let ac = new AccountEntity()
-
 
         /*-----------------------------------------------------------------------------------------*/
 
@@ -36,22 +29,31 @@ const app = async () =>{
             }
         }
 
+        /*-----------------------------------------------------------------------------------------*/
 
-
+        //Função para imprimir o Menu de acesso à conta
         function printMenuAccount() {
             console.log("-----------------CONTAS-----------------")
             console.log("1 - CADASTRO")
-            console.log("2 - VER DADOS CADASTRADOS")
-            console.log("3 - ACESSAR FUNCIONALIDADES")
-            console.log("4 - REMOVER CONTA CADASTRADA")
-            console.log("5 - EDITAR DADOS CADASTRADOS")
-            console.log("6 - VER CONTAS CADASTRADAS")
-            console.log("7 - SAIR")
+            console.log("2 - ACESSAR CONTA")
+            console.log("3 - SAIR")
 
             option = Number(prompt("Opção escolhida: "))
         }
 
-        // Função para imprimir o menu e pegar a opção escolhida
+        // Função para imprimir o menu com as opções da conta em uso
+        function printMenuUser() {
+            console.log("------------------OLÁ------------------")
+            console.log("1 - ACESSAR FUNCIONALIDADES")
+            console.log("2 - VER DADOS CADASTRADOS")
+            console.log("3 - REMOVER CONTA CADASTRADA")
+            console.log("4 - EDITAR DADOS CADASTRADOS")
+            console.log("5 - SAIR")
+
+            option = Number(prompt("Opção escolhida: "))
+        }
+
+        // Função para imprimir o menu de funcionalidades
         function printMenu() {
             console.log("------------------MENU------------------")
             console.log("1 - ADICIONAR GASTO")
@@ -72,128 +74,130 @@ const app = async () =>{
 
         // Começo do programa
         printLine(40,2)
-        let accounts: Array<Account> = [] // NÃO É NECESSÁRIO
         let acc = new Account // não fica mais aqui
 
-        do {
-            printMenuAccount()
-
-            switch (option) {
-                case 1:
-                    console.log("\n~~~~~~~~~~ADICIONAR CONTA~~~~~~~~~~\n")
-                    accounts.push(acc.createAccount("idAcc"))
-                    console.log(acc)
-                    acc.CPF = prompt("CPF: ")
-                    acc.Name = prompt("Nome: ")
-                    const a = await accountRepository.create(acc)
-                    console.log(a)
-
-                    idAcc++
-                    break;
-                case 2:
-                    if (accounts.length === 0){
-                        console.log(`\nPor favor, crie uma conta primeiro!`)
-                    } else {
-                        printLine(40, 1)
-                        acc.showDataAccount(accounts)
-                        printLine(40, 1)
-                    }
-                    
-                    break;
-                case 3:
-                    if (accounts.length === 0){
-                        console.log(`\nPor favor, crie uma conta primeiro!`)
-                        option = 0
-                    }
-                    break;
-                case 4:
-                    if (accounts.length === 0){
-                        console.log(`\nPor favor, crie uma conta primeiro!`)
-                        option = 0
-                    }
-                    let id = prompt("ID da Conta: ")
-                    const b = await accountRepository.delete(id)
-                    console.log(b)
-                    break;
-                case 5:
-                    if (accounts.length === 0){
-                        console.log(`\nPor favor, crie uma conta primeiro!`)
-                        option = 0
-                    }
-                    let identifier = prompt("ID da Conta: ")
-                    let CPF = prompt("Novo CPF da conta: ")
-                    let Nome = prompt("Novo NOME da conta: ")
-                    const c = await accountRepository.update(identifier, CPF, Nome)
-                    console.log(c)
-                    break;
-                case 6:
-                    if (accounts.length === 0){
-                        console.log(`\nPor favor, crie uma conta primeiro!`)
-                        option = 0
-                    }
-                    const d = await accountRepository.get_all()
-                    break;
-
-                default:
-                    console.log("\nOpção incorreta! Siga o menu!")
-                    break;
-            }
-        } while (option != 7)
-
-        printLine(40,2)
-        option = 0
-
-        do{
-            printMenu()
-            printLine(40, 1)
-            switch (option) {
-                case 1:
-                    console.log("~~~~~~~~~~ADICIONAR GASTO~~~~~~~~~~\n")
-                    acc.addOutput(accounts[0], idOutflow)
-                    idOutflow++
-                    break;
-                case 2:
-                    console.log("~~~~~~~~~~REMOVER GASTO~~~~~~~~~~\n")
-                    acc.rmOutput(accounts[0])
-                    break;
-                case 3:
-                    console.log("~~~~~~~~~~LISTAR GASTOS~~~~~~~~~~\n")
-                    acc.listOutput(accounts[0])
-                    break;
-                case 4:
-                    console.log("~~~~~~~~~~EDITAR GASTOS~~~~~~~~~~\n")
-                    acc.editOutput(accounts[0])
-                    break;
-                case 5:
-                    console.log("~~~~~~~~~~ADICIONAR GANHO~~~~~~~~~~\n")
-                    acc.addInput(accounts[0], idInflow)
-                    idInflow++
-                    break;
-                case 6:
-                    console.log("~~~~~~~~~~REMOVER GANHO~~~~~~~~~~\n")
-                    acc.rmInput(accounts[0])
-                    break;
-                case 7:
-                    console.log("~~~~~~~~~~LISTAR GANHOS~~~~~~~~~~\n")
-                    acc.listInput(accounts[0])
-                    break;
-                case 8:
-                    console.log("~~~~~~~~~~EDITAR GANHO~~~~~~~~~~\n")
-                    acc.editInput(accounts[0])
-                    break;
-                case 9:
-                    console.log("~~~~~~~~~~GERAR EXTRATO~~~~~~~~~~\n")
-                    acc.genStats(accounts[0])
-                    break;
-                case 10:
-                    console.log("OBRIGADO POR UTILIZAR O PROGRAMA!\n")
-                    break;
-                default:
-                    console.log("OPÇÃO INCORRETA! SIGA O MENU E DIGITE NOVAMENTE!\n")
-                    break;
-            }
-            printLine(40, 2)
-        } while (option != 10)
+        // 
 }
 
 export default app
+
+
+// do {
+    //     printMenuAccount()
+
+    //     switch (option) {
+    //         case 1:
+    //             console.log("\n~~~~~~~~~~ADICIONAR CONTA~~~~~~~~~~\n")
+    //             accounts.push(acc.createAccount("idAcc"))
+    //             console.log(acc)
+    //             acc.CPF = prompt("CPF: ")
+    //             acc.Name = prompt("Nome: ")
+    //             const a = await accountRepository.create(acc)
+    //             console.log(a)
+
+    //             idAcc++
+    //             break;
+    //         case 2:
+    //             if (accounts.length === 0){
+    //                 console.log(`\nPor favor, crie uma conta primeiro!`)
+    //             } else {
+    //                 printLine(40, 1)
+    //                 acc.showDataAccount(accounts)
+    //                 printLine(40, 1)
+    //             }
+                
+    //             break;
+    //         case 3:
+    //             if (accounts.length === 0){
+    //                 console.log(`\nPor favor, crie uma conta primeiro!`)
+    //                 option = 0
+    //             }
+    //             break;
+    //         case 4:
+    //             if (accounts.length === 0){
+    //                 console.log(`\nPor favor, crie uma conta primeiro!`)
+    //                 option = 0
+    //             }
+    //             let id = prompt("ID da Conta: ")
+    //             const b = await accountRepository.delete(id)
+    //             console.log(b)
+    //             break;
+    //         case 5:
+    //             if (accounts.length === 0){
+    //                 console.log(`\nPor favor, crie uma conta primeiro!`)
+    //                 option = 0
+    //             }
+    //             let identifier = prompt("ID da Conta: ")
+    //             let CPF = prompt("Novo CPF da conta: ")
+    //             let Nome = prompt("Novo NOME da conta: ")
+    //             const c = await accountRepository.update(identifier, CPF, Nome)
+    //             console.log(c)
+    //             break;
+    //         case 6:
+    //             if (accounts.length === 0){
+    //                 console.log(`\nPor favor, crie uma conta primeiro!`)
+    //                 option = 0
+    //             }
+    //             const d = await accountRepository.get_all()
+    //             break;
+
+    //         default:
+    //             console.log("\nOpção incorreta! Siga o menu!")
+    //             break;
+    //     }
+    // } while (option != 7)
+
+    // printLine(40,2)
+    // option = 0
+
+    // do{
+    //     printMenu()
+    //     printLine(40, 1)
+    //     switch (option) {
+    //         case 1:
+    //             console.log("~~~~~~~~~~ADICIONAR GASTO~~~~~~~~~~\n")
+    //             acc.addOutput(accounts[0], idOutflow)
+    //             idOutflow++
+    //             break;
+    //         case 2:
+    //             console.log("~~~~~~~~~~REMOVER GASTO~~~~~~~~~~\n")
+    //             acc.rmOutput(accounts[0])
+    //             break;
+    //         case 3:
+    //             console.log("~~~~~~~~~~LISTAR GASTOS~~~~~~~~~~\n")
+    //             acc.listOutput(accounts[0])
+    //             break;
+    //         case 4:
+    //             console.log("~~~~~~~~~~EDITAR GASTOS~~~~~~~~~~\n")
+    //             acc.editOutput(accounts[0])
+    //             break;
+    //         case 5:
+    //             console.log("~~~~~~~~~~ADICIONAR GANHO~~~~~~~~~~\n")
+    //             acc.addInput(accounts[0], idInflow)
+    //             idInflow++
+    //             break;
+    //         case 6:
+    //             console.log("~~~~~~~~~~REMOVER GANHO~~~~~~~~~~\n")
+    //             acc.rmInput(accounts[0])
+    //             break;
+    //         case 7:
+    //             console.log("~~~~~~~~~~LISTAR GANHOS~~~~~~~~~~\n")
+    //             acc.listInput(accounts[0])
+    //             break;
+    //         case 8:
+    //             console.log("~~~~~~~~~~EDITAR GANHO~~~~~~~~~~\n")
+    //             acc.editInput(accounts[0])
+    //             break;
+    //         case 9:
+    //             console.log("~~~~~~~~~~GERAR EXTRATO~~~~~~~~~~\n")
+    //             acc.genStats(accounts[0])
+    //             break;
+    //         case 10:
+    //             console.log("OBRIGADO POR UTILIZAR O PROGRAMA!\n")
+    //             break;
+    //         default:
+    //             console.log("OPÇÃO INCORRETA! SIGA O MENU E DIGITE NOVAMENTE!\n")
+    //             break;
+    //     }
+    //     printLine(40, 2)
+    // } while (option != 10)
--- src/service/financialStatsService.ts
+++ src/service/financialStatsService.ts
@@ -1,99 +1,100 @@
-// Importações
-import { Input } from '../model/CashInflow'
-import { Output } from '../model/CashOutflow'
+// NÃO FUNCIONAL NESSA NOVA VERSÃO DO PROGRAMA
+// // Importações
+// import { Input } from '../model/CashInflow'
+// import { Output } from '../model/CashOutflow'
 
 
-// npm install prompt-sync
-const prompt = require("prompt-sync")()
-let inpt = new Input()
-let otpt = new Output()
+// // npm install prompt-sync
+// const prompt = require("prompt-sync")()
+// let inpt = new Input()
+// let otpt = new Output()
 
-export class financialStats {
+// export class financialStats {
 
-    constructor () {}
+//     constructor () {}
 
-    // Função que gera um extrato com gastos e ganhos
-    generateStats(inflow: Array<Input>, outflow: Array<Output>) {
-        let sumOutput = 0
-        let gastoEdu = 0, gastoEnt = 0, gastoSau = 0, gastoTra = 0
-        let sumInput = 0
+//     // Função que gera um extrato com gastos e ganhos
+//     generateStats(inflow: Array<Input>, outflow: Array<Output>) {
+//         let sumOutput = 0
+//         let gastoEdu = 0, gastoEnt = 0, gastoSau = 0, gastoTra = 0
+//         let sumInput = 0
 
-        console.log("> BALANÇO DA CARTEIRA <")
+//         console.log("> BALANÇO DA CARTEIRA <")
         
-        // Somando gastos com o for(let ... of ...)
-        // for (let value of outflow) {
-        //     sumOutput += value.valor
-        //     if (value.area == "Alimentação") {
-        //         gastoAli += value.valor
-        //     } else if (value.area == "Educação") {
-        //         gastoEdu += value.valor
-        //     } else if (value.area == "Entretenimento") {
-        //         gastoEnt += value.valor
-        //     } else if (value.area == "Saúde") {
-        //         gastoSau += value.valor
-        //     } else if (value.area == "Transporte") {
-        //         gastoTra += value.valor
-        //     }
-        // }
+//         // Somando gastos com o for(let ... of ...)
+//         // for (let value of outflow) {
+//         //     sumOutput += value.valor
+//         //     if (value.area == "Alimentação") {
+//         //         gastoAli += value.valor
+//         //     } else if (value.area == "Educação") {
+//         //         gastoEdu += value.valor
+//         //     } else if (value.area == "Entretenimento") {
+//         //         gastoEnt += value.valor
+//         //     } else if (value.area == "Saúde") {
+//         //         gastoSau += value.valor
+//         //     } else if (value.area == "Transporte") {
+//         //         gastoTra += value.valor
+//         //     }
+//         // }
 
-        // Somando gastos usando forEach
-        outflow.forEach(element => {
-            sumOutput += element.valor
-            if (element.area == "Educação") {
-                gastoEdu += element.valor
-            } else if (element.area == "Entretenimento") {
-                gastoEnt += element.valor
-            } else if (element.area == "Saúde") {
-                gastoSau += element.valor
-            } else if (element.area == "Transporte") {
-                gastoTra += element.valor
-            }
-        });
+//         // Somando gastos usando forEach
+//         outflow.forEach(element => {
+//             sumOutput += element.valor
+//             if (element.area == "Educação") {
+//                 gastoEdu += element.valor
+//             } else if (element.area == "Entretenimento") {
+//                 gastoEnt += element.valor
+//             } else if (element.area == "Saúde") {
+//                 gastoSau += element.valor
+//             } else if (element.area == "Transporte") {
+//                 gastoTra += element.valor
+//             }
+//         });
 
-        // Criando um vetor com todos os valores gastos em alimentação
-        let gastoAli = outflow.map(function(outflow){
-            if(outflow.area == "Alimentação"){
-                return outflow.valor
-            }
-        });
+//         // Criando um vetor com todos os valores gastos em alimentação
+//         let gastoAli = outflow.map(function(outflow){
+//             if(outflow.area == "Alimentação"){
+//                 return outflow.valor
+//             }
+//         });
 
-        // Somando gastos do vetor de gastos de alimentação
-        let somaAli = 0
-        for(let value of gastoAli){
-            if(value != null){
-                somaAli += value
-            }
-        } 
+//         // Somando gastos do vetor de gastos de alimentação
+//         let somaAli = 0
+//         for(let value of gastoAli){
+//             if(value != null){
+//                 somaAli += value
+//             }
+//         } 
 
-        console.log(`\nNo total, foram gastos R$ ${sumOutput.toFixed(2)}, sendo distribuídos nas seguintes áreas: `)
-        console.log(`ALIMENTAÇÃO: R$ ${somaAli.toFixed(2)}`)
-        console.log(`EDUCAÇÃO: R$ ${gastoEdu.toFixed(2)}`)
-        console.log(`ENTRETENIMENTO: R$ ${gastoEnt.toFixed(2)}`)
-        console.log(`SAÚDE: R$ ${gastoSau.toFixed(2)}`)
-        console.log(`TRANSPORTE: R$ ${gastoTra.toFixed(2)}`)
+//         console.log(`\nNo total, foram gastos R$ ${sumOutput.toFixed(2)}, sendo distribuídos nas seguintes áreas: `)
+//         console.log(`ALIMENTAÇÃO: R$ ${somaAli.toFixed(2)}`)
+//         console.log(`EDUCAÇÃO: R$ ${gastoEdu.toFixed(2)}`)
+//         console.log(`ENTRETENIMENTO: R$ ${gastoEnt.toFixed(2)}`)
+//         console.log(`SAÚDE: R$ ${gastoSau.toFixed(2)}`)
+//         console.log(`TRANSPORTE: R$ ${gastoTra.toFixed(2)}`)
 
-        for (let value of inflow) {
-            sumInput += value.valor
-        }
+//         for (let value of inflow) {
+//             sumInput += value.valor
+//         }
 
-        console.log(`\nNo total, foram recebidos R$ ${sumInput.toFixed(2)}, sendo distribuídos em: `)
-        for (let value of inflow) {
-            console.log(`- ${value.titulo}: R$ ${value.valor.toFixed(2)}`)
-        }
+//         console.log(`\nNo total, foram recebidos R$ ${sumInput.toFixed(2)}, sendo distribuídos em: `)
+//         for (let value of inflow) {
+//             console.log(`- ${value.titulo}: R$ ${value.valor.toFixed(2)}`)
+//         }
         
-        let balanco = Number(sumInput) - Number(sumOutput)
-        console.log(`\nPortanto, seu balanço geral da carteira é de R$ ${balanco.toFixed(2)}\n`)
+//         let balanco = Number(sumInput) - Number(sumOutput)
+//         console.log(`\nPortanto, seu balanço geral da carteira é de R$ ${balanco.toFixed(2)}\n`)
         
 
-        let yesNo = prompt("(S/N) Deseja exibir a lista de todos os ganhos? ")
-        if(yesNo == 'S' || yesNo == 's' || yesNo == 'Y' || yesNo == 'y') {
-            inpt.listarGanhos(inflow)
-        }
+//         let yesNo = prompt("(S/N) Deseja exibir a lista de todos os ganhos? ")
+//         if(yesNo == 'S' || yesNo == 's' || yesNo == 'Y' || yesNo == 'y') {
+//             inpt.listarGanhos(inflow)
+//         }
 
-        yesNo = prompt("(S/N) Deseja exibir a lista de todos os gastos? ")
-        if(yesNo == 'S' || yesNo == 's' || yesNo == 'Y' || yesNo == 'y') {
-            otpt.listarGastos(outflow)
-        }
-    }
+//         yesNo = prompt("(S/N) Deseja exibir a lista de todos os gastos? ")
+//         if(yesNo == 'S' || yesNo == 's' || yesNo == 'Y' || yesNo == 'y') {
+//             otpt.listarGastos(outflow)
+//         }
+//     }
     
-}
+// }
