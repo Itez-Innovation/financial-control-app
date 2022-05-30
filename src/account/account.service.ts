2b4507a2a1aef9dceaaddfb238c6241ef82c06bf
gabrielm2q <mariano.gabriel2q@gmail.com>
2022-05-30 14:17:16 -0300

    :repeat: updating account service and package

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { AccountEntity } from './entity/account.entity';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  constructor(
+    @InjectModel(AccountEntity)
+    private accountEntity: typeof AccountEntity,
+  ) {}
+}
