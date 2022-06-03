4d9ac3772552de0b5a3edc58ee427500410c58a0
gabrielm2q <mariano.gabriel2q@gmail.com>
2022-06-03 17:46:54 -0300

    :repeat: services and controllers - roles

--- src/controller/role.controller.ts
+++ src/controller/role.controller.ts
@@ -1,35 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { RoleService } from 'src/service/role.service';
 
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
-  async create() {
-    try {
-      const { name, description } = request.body;
+  @Post('create')
+  async create(@Body() createData: { name: string; description: string }) {
+    const { name, description } = createData;
 
-      const response = await service.create({ name, description });
-
-      res.status(201).json(response);
-    } catch (error) {
-      res.status(400).json({ code: 400, message: 'internal server error' });
-    }
+    return this.roleService.create({
+      name,
+      description,
+    });
   }
 
-  async createRolePermission() {
-    try {
-      const { roleId } = request.params;
-      const { permissions } = request.body;
-
-      const response = await service.createRolePermission({
-        roleId,
-        permissions,
-      });
-
-      res.status(201).json(response);
-    } catch (error) {
-      res.status(400).json({ code: 400, message: 'internal server error' });
-    }
+  @Post('permission/:id')
+  async createRolePermission(
+    @Param('id') id: string,
+    @Body() permissions: string[],
+  ) {
+    return this.roleService.createRolePermission({ roleId: id, permissions });
   }
 }
--- src/service/role.service.ts
+++ src/service/role.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import CustomError from '../exceptions/custom.error';
+import ConflictError from '../exceptions/conflict.error';
+import { PrismaService } from './prisma.service';
+import NotFoundError from '../exceptions/not-found.error';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(private prisma: PrismaService) {}
+
+  async create({ name, description }) {
+    try {
+      if (await this.findByName(name))
+        throw new ConflictError(`This role already exists`);
+
+      return this.prisma.roles.create({
+        data: { name, description },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      else throw new Error(`Internal server error`);
+    }
+  }
+
+  async createRolePermission({ roleId, permissions }) {
+    try {
+      const role = await this.findById(roleId);
+
+      if (!role) throw new NotFoundError('Role does not exists!');
+
+      const permissionsExist = await this.prisma.roles.findMany({
+        select: { id: permissions },
+      });
+
+      if (!permissionsExist)
+        throw new NotFoundError('Permissions does not exists!');
+
+      return await this.prisma.roles.update({
+        where: { id: roleId },
+        data: { permissions: permissions },
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      else throw new Error('Internal server error');
+    }
+  }
+
+  async findByName(name: string) {
+    return this.prisma.roles.findFirst({
+      where: { name: name },
+    });
+  }
+
+  async findById(id: string) {
+    return this.prisma.roles.findFirst({
+      where: { id: id },
+    });
+  }
+}
