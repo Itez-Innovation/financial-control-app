8e925f5df5f4c947256267814f1bf136a461d56b
gabrielm2q <mariano.gabriel2q@gmail.com>
2022-06-10 12:43:41 -0300

    :repeat: creating role guards (II)

--- src/middleware/roles.guard.ts
+++ src/middleware/roles.guard.ts
@@ -1,11 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return matchRoles(roles, user.roles);
   }
 }
